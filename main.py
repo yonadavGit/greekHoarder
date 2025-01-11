99b89fbc1f4dc2e6c231e03d6813ca474d5c650e
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2025-01-11 22:41:43 +0200

    first pass at querying an entire book

--- main.py
+++ main.py
@@ -6,31 +6,64 @@ def parse_response(html):
     raw_text = soup.get_text()
     return raw_text
 
-def query_perseus(text_id, book, chapter, section, num):
+def query_perseus(text_id, book, chapter, section):
     base_url = "https://www.perseus.tufts.edu/hopper/loadquery"
-    params = {
-        "doc": f"Perseus:text:{text_id}",
-        "book": book,
-        "chapter": chapter,
-        "section": section,
-        "num": num
-    }
-    response = requests.get(base_url, params=params)
+    # params = {
+    #     "doc": f"Perseus:text:{text_id}",
+    #     "book": book,
+    #     "chapter": chapter,
+    #     "section": section,
+    # }
+    response = requests.get(f"{base_url}?doc=Perseus:text:{text_id}:book={book}:chapter={chapter}:section={section}")
     response.raise_for_status()
     return parse_response(response.text)
 
+class BookNavigator:
+    def __init__(self):
+        self.book = 1
+        self.chapter = 1
+        self.section = 1
 
+    def next_section(self):
+        self.section += 1
+
+    def next_chapter(self):
+        self.chapter += 1
+        self.section = 1  # Reset section to 1 when moving to the next chapter
+
+    def next_book(self):
+        self.book += 1
+        self.chapter = 1  # Reset chapter to 1 when moving to the next book
+        self.section = 1  # Reset section to 1 when moving to the next book
+
+    def as_dict(self):
+        return {"book": self.book, "chapter":self.chapter, "section":self.section}
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main_text_perseus_id = "1999.01.0207"
     secondary_text_perseus_id = "1999.01.0208"
-    doc = main_text_perseus_id
-    book = "1"
-    chapter = "1"
-    section = "20"
-    num = 1
-    text = query_perseus(main_text_perseus_id, book, chapter, section, num)
-    print(text)
+    nav = BookNavigator()
+    while True:
+        try:
+            text = query_perseus(main_text_perseus_id, nav.book, nav.chapter, nav.section)
+            print(text)
+            nav.next_section()
+        except:
+            try:
+                nav.next_chapter()
+                text = query_perseus(main_text_perseus_id, nav.book, nav.chapter, nav.section)
+                print(text)
+                nav.next_section()
+            except:
+                try:
+                    nav.next_book()
+                    text = query_perseus(main_text_perseus_id, nav.book, nav.chapter, nav.section)
+                    print(text)
+                    nav.next_section()
+                except:
+                    print("end")
+
+
 
